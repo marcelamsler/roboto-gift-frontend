1653d92c5ac3ced1fabdffe689b69e5a191c9576
Marcel Amsler <marcel.amsler@gmail.com>
2017-09-15 22:43:41 +0200

    added menu instead of users

--- app/src/users/Users.js
+++ app/src/users/Users.js
@@ -2,7 +2,6 @@
 
 import UsersDataService from 'src/users/services/UsersDataService';
 
-import UsersList from 'src/users/components/list/UsersList';
 import UserDetails from 'src/users/components/details/UserDetails';
 
 // Define the Angular 'users' module
@@ -10,7 +9,6 @@ import UserDetails from 'src/users/components/details/UserDetails';
 export default angular
   .module("users", ['ngMaterial'])
 
-  .component(UsersList.name, UsersList.config)
   .component(UserDetails.name, UserDetails.config)
 
   .service("UsersDataService", UsersDataService);
