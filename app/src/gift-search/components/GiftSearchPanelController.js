cc5d3148e4c407ba6ef237cc93eb31a3eeeb590d
Styp <mstypinski@gmail.com>
2017-09-16 08:23:13 +0200

    added some shit

--- app/src/gift-search/components/GiftSearchPanelController.js
+++ app/src/gift-search/components/GiftSearchPanelController.js
@@ -1,8 +1,17 @@
 class GiftSearchPanelController {
 
-  constructor($mdBottomSheet, $log) {
-    this.$mdBottomSheet = $mdBottomSheet;
-    this.$log = $log;
-  }
+    constructor(GiftSearchService, $mdBottomSheet, $log) {
+        this.$mdBottomSheet = $mdBottomSheet;
+        this.$log = $log;
+    }
+
+    sendLogin() {
+
+        GiftSearchService.getGiftRecommendations(this.username, this.userpassword, this.targetuser, 3);
+
+    }
+
+
 }
+
 export default GiftSearchPanelController;
